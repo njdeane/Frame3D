1c110059e190e810225c6bd88ed11e88da95a76f
Nic Deane <njdeane7@gmail.com>
2022-01-24 16:19:53 +1000

    Initial Commit

---
+++ Shared/Frame3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  Frame3DApp.swift
+//  Shared
+//
+//  Created by Nic Deane on 24/1/2022.
+//
+
+import SwiftUI
+
+@main
+struct Frame3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
